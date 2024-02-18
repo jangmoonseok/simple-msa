39ea28e0fd7c96bffc656e834d509ea5ba44a9ce
jangmoonseok <cacao621@naver.com>
2024-02-18 22:11:31 +0900

    UserEntity Test

---
+++ UserService/src/main/java/com/msa/user/UserRepository.java
@@ -0,0 +1,22 @@
+package com.msa.user;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
+public class UserRepository {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	public Long save(UserEntity user) {
+		em.persist(user);
+		return user.getId();
+	}
+	
+	public UserEntity find(Long id) {
+		return em.find(UserEntity.class, id);
+	}
+}
